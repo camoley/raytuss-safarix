503ce7722e81fccc77ee229af3b67e7c4485e927
camoley <34395516+camoley@users.noreply.github.com>
2019-12-08 16:40:19 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args, ops) => {
         const sec = Math.floor((ms / 1000) % 60).toString()
         const min = Math.floor((ms / (1000 * 60)) % 60).toString()
         const hrs = Math.floor((ms / (1000 * 60 *60)) % 60).toString()
-        const days = Math.floor((ms / (1000 * 60 * 60 *24)) % 60).toString()
+        const days = Math.floor((ms / (1000 * 60 * 60 *24)) % 24).toString()
         return `${days.padStart(1,'0')} gün, ${hrs.padStart(2,'0')} saat, ${min.padStart(2,'0')} dakika , ${sec.padStart(2,'0')} saniye`
     }
 
